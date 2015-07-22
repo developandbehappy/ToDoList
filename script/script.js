2cbfb1577675ea998e782340edfe152cb8082ced
zidein07 <zidein07@gmail.com>
2015-07-22 18:33:55 +0400

    new

--- script/script.js
+++ script/script.js
@@ -5,18 +5,29 @@ var press = document.getElementById("add");
 var text = document.getElementById("text");
 var block = document.getElementById("block");
 var blockOne = document.getElementById("blockOne");
+  var ul = document.getElementById("ul");
+  var link = document.createElement("li");
+  var a = document.createElement("a");
 
-
+var ToDoStorage = function(arr,addNew){
+  this.addEverything = function(arr) {
+    for(i = 0; i < arr.length; i++){
+        ul.appendChild(link).appendChild(a).innerHTML = arr[i];
+    }
+  }
+  this.lastItem = function(arr){
+    arr.pop();
+  }
+  this.addItem = function(addNew){
+    arr.push(text.value);
+  }
+}
 
 
 
 
 press.onclick = init;
-  
 if(JSON.parse(localStorage.a).length > 0){
-  var ul = document.getElementById("ul");
-  var link = document.createElement("li");
-  var a = document.createElement("a");
   for(i = 0; i < JSON.parse(localStorage.a).length; i++){
     ul.appendChild(link).appendChild(a).innerHTML = JSON.parse(localStorage.a)[i];
   }
@@ -28,7 +39,6 @@ if(JSON.parse(localStorage.a).length > 0){
         textBox.push(text.value);
         text.value = '';
         addLocal();
-
     }
       else{
        
@@ -43,9 +53,6 @@ function addLocal(){
 
 function setItem(item){
   var push = JSON.parse(item);
-  var ul = document.getElementById("ul");
-  var link = document.createElement("li");
-  var a = document.createElement("a");
   for(i = 0; i < push.length; i++){
     ul.appendChild(link).appendChild(a).innerHTML = push[i];
   }
