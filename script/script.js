518faa9cbb3b81c10376c9d030d30379d9a41769
zidein07 <zidein07@gmail.com>
2015-07-22 18:13:03 +0400

    new but have some problems. I change now it dnt worry :D

--- script/script.js
+++ script/script.js
@@ -13,11 +13,21 @@ var blockOne = document.getElementById("blockOne");
 
 press.onclick = init;
   
+if(JSON.parse(localStorage.a).length > 0){
+  var ul = document.getElementById("ul");
+  var link = document.createElement("li");
+  var a = document.createElement("a");
+  for(i = 0; i < JSON.parse(localStorage.a).length; i++){
+    ul.appendChild(link).appendChild(a).innerHTML = JSON.parse(localStorage.a)[i];
+  }
+}
+
   function init(){
     if(text.value !== ''){
+
         textBox.push(text.value);
         text.value = '';
-        add(text.value);
+        addLocal();
 
     }
       else{
@@ -26,14 +36,17 @@ press.onclick = init;
   }
 
 
-function add(txt){
-    var ul = document.getElementById("ul");
-    var link = document.createElement("li");
-    var a = document.createElement("a");
-    // StringArray = JSON.stringify(textBox);
-    // Storage.getItem(stringArray);
-   for(i = 0; i<textBox.length; i++){
-          ul.appendChild(link).appendChild(a).innerHTML = textBox[i];
-    }
+function addLocal(){
+    localStorage.a = JSON.stringify(textBox);
+    setItem(localStorage.a);
+}
 
+function setItem(item){
+  var push = JSON.parse(item);
+  var ul = document.getElementById("ul");
+  var link = document.createElement("li");
+  var a = document.createElement("a");
+  for(i = 0; i < push.length; i++){
+    ul.appendChild(link).appendChild(a).innerHTML = push[i];
+  }
 }
