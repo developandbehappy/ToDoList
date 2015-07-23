0b927179c2a75a968cee9abfe2c24fd6bbbc8727
zidein07 <zidein07@gmail.com>
2015-07-23 15:45:40 +0400

    new

--- script/script.js
+++ script/script.js
@@ -5,58 +5,105 @@ var press = document.getElementById("add");
 var text = document.getElementById("text");
 var block = document.getElementById("block");
 var blockOne = document.getElementById("blockOne");
-  var ul = document.getElementById("ul");
-  var link = document.createElement("li");
-  var a = document.createElement("a");
+var ul = document.getElementById("ul");
 
-var ToDoStorage = function(arr,addNew){
-  this.addEverything = function(arr) {
-    for(i = 0; i < arr.length; i++){
-        ul.appendChild(link).appendChild(a).innerHTML = arr[i];
-    }
-  },
-  this.lastItem = function(arr){
-    arr.pop();
-  },
-  this.addItem = function(addNew){
-    arr.push(text.value);
-  },
-  this.removeAll = function(arr){
-       for(i = 0; i < arr.length; i++){
-        arr.shift();
-    }
-  },
-    this.removeItem = function(){
-
-    }
-}
-
-
-
-
+window.onload = Prov;
 press.onclick = init;
 
-  function init(){
-    if(text.value !== ''){
 
-        textBox.push(text.value);
-        text.value = '';
-        addLocal();
+function init(){
+  if(text.value !== ''){
+    if(TodoStorage.getAll()){
+      TodoStorage.addItem(text.value);
+      addVis(text.value);
+      text.value = '';
+
     }
-      else{
-       
-      }
+    else
+    {
+      TodoStorage.create();
+      TodoStorage.addItem(text.value);
+    }
   }
-
-
-function addLocal(){
-    localStorage.a = JSON.stringify(textBox);
-    setItem(localStorage.a);
 }
 
-function setItem(item){
-  var push = JSON.parse(item);
-  for(i = 0; i < push.length; i++){
-    ul.appendChild(link).appendChild(a).innerHTML = push[i];
+
+
+/**
+ * Функция для проверки строки на JSON
+ * @param str
+ * @returns {boolean}
+ * @constructor
+ */
+var IsJsonString = function (str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
   }
+  return true;
+};
+
+/**
+ *
+ * @type {{storage: Storage, setObject: Function, setString: Function, get: Function}}
+ */
+var StorageHelper = {
+  storage: window.localStorage,
+  setObject: function (name, object) {
+    var string = JSON.stringify(object);
+    this.storage.setItem(name, string);
+  },
+  setString: function (name, string) {
+    this.storage.setItem(name, string);
+  },
+  get: function (name) {
+    var result = this.storage.getItem(name);
+    if (!result) {
+      return false;
+    }
+    if (IsJsonString(result)) {
+      return JSON.parse(result);
+    }
+    return result;
+  }
+};
+
+var TodoStorage = {
+  storageName: 'todo_storage',
+  create: function () {
+    if (this.getAll()) {
+      return false;
+    }
+    StorageHelper.setObject(this.storageName, []);
+  },
+  getAll: function () {
+    return StorageHelper.get(this.storageName);
+  },
+  addItem: function (item) {
+    var arr = this.getAll();
+    arr.push(item);
+    StorageHelper.setObject(this.storageName, arr);
+  }
+};
+
+function Prov(){
+  if(TodoStorage.getAll()){
+  for(i = 0; i < TodoStorage.getAll().length; i++){
+      var link = document.createElement("li");
+      var a = document.createElement("a");
+      ul.appendChild(link).appendChild(a).innerHTML = TodoStorage.getAll()[i];
+  }
+}
+else
+  {
+    TodoStorage.create();
+  }
+}
+
+
+function addVis(text){
+      var link = document.createElement("li");
+      var a = document.createElement("a");
+      ul.appendChild(link).appendChild(a).innerHTML = text;
 }
