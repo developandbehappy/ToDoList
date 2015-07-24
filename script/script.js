cd77a33aacd9530dd1269a42d7fbc30e57732fdd
zidein07 <zidein07@gmail.com>
2015-07-24 16:36:01 +0400

    @avevlad Bootstrap

--- script/script.js
+++ script/script.js
@@ -31,63 +31,7 @@ function init(){
 
 
 
-/**
- * Функция для проверки строки на JSON
- * @param str
- * @returns {boolean}
- * @constructor
- */
-var IsJsonString = function (str) {
-  try {
-    JSON.parse(str);
-  } catch (e) {
-    return false;
-  }
-  return true;
-};
 
-/**
- *
- * @type {{storage: Storage, setObject: Function, setString: Function, get: Function}}
- */
-var StorageHelper = {
-  storage: window.localStorage,
-  setObject: function (name, object) {
-    var string = JSON.stringify(object);
-    this.storage.setItem(name, string);
-  },
-  setString: function (name, string) {
-    this.storage.setItem(name, string);
-  },
-  get: function (name) {
-    var result = this.storage.getItem(name);
-    if (!result) {
-      return false;
-    }
-    if (IsJsonString(result)) {
-      return JSON.parse(result);
-    }
-    return result;
-  }
-};
-
-var TodoStorage = {
-  storageName: 'todo_storage',
-  create: function () {
-    if (this.getAll()) {
-      return false;
-    }
-    StorageHelper.setObject(this.storageName, []);
-  },
-  getAll: function () {
-    return StorageHelper.get(this.storageName);
-  },
-  addItem: function (item) {
-    var arr = this.getAll();
-    arr.push(item);
-    StorageHelper.setObject(this.storageName, arr);
-  }
-};
 
 function Prov(){
   if(TodoStorage.getAll()){
