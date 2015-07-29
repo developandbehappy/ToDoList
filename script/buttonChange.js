416a2982b58d658daae8b139c9f5f187d32b2ba4
zidein07 <zidein07@gmail.com>
2015-07-29 09:58:42 +0400

    minCode with tab and add ForEach

--- script/buttonChange.js
+++ script/buttonChange.js
@@ -1,77 +1,1 @@
-var active = document.getElementById('active');
-var done = document.getElementById('did');
-var remove = document.getElementById('remove');
-
-var act = false,dn = false,rem = false;
-
-
-active.onclick = function(){
-	act = true;
-	dn = false;
-	rem = false;
-	stateOn();
-	console.log('Its Active Block');
-}
-
-done.onclick = function(){
-	act = false;
-	dn = true;
-	rem = false;
-	stateOn();
-	console.log('Its Done Block');
-}
-
-remove.onclick = function(){
-	act = false;
-	dn = false;
-	rem = true;
-	stateOn();
-	console.log('Its remove Block');
-}
-
-function stateOn(){
-  stateIn = TodoStorage.data;
-  deleteLabel();
-  for (i = 0;i < stateIn.length; i++){
-    if (act){
-      if(stateIn[i].state == 'active'){
-	      labelTake();
-  		}
-    }
-    if (dn){
-      if(stateIn[i].state == 'done'){
-	      labelTake();
-	      x.setAttribute("checked", true);
-          lineThrough(label);
-  		}
-    }
-    if (rem){
-      if(stateIn[i].state == 'remove'){
-	      labelTake();
-  		}
-    }
-      function labelTake(){
-		  link = document.createElement("li");
-		  x = document.createElement("INPUT");
-		  label = document.createElement("label");
-		  x.setAttribute("type", "checkbox");
-	      label.setAttribute("for", "c" + i);
-	      label.setAttribute("id", "cs" + i);
-	      x.setAttribute("id", "c" + i);
-	      x.setAttribute("onclick", "changeBox(" + i + ")");
-	      ul.appendChild(link).appendChild(x);
-	      ul.appendChild(link).appendChild(label).innerHTML = stateIn[i].title;
-    }
-  }
-}
-
-function deleteLabel(){
-    if(ul.childNodes.length > 0){
-  	 for (i = 0;i < ul.childNodes.length; i++){
-		 x = document.getElementById("c"+i);
-		 label = document.getElementById("cs"+i);
-		 ul.removeChild(ul.childNodes[i]);
-		 deleteLabel();
-	  }
-	}
-}
+active = document.getElementById('active'),done = document.getElementById('did'),remove = document.getElementById('remove');act = false,dn = false,rem = false;active.onclick = function() {	act = true;	dn = false;	rem = false;	stateOn();	console.log('Its Active Block');}done.onclick = function() {	act = false;	dn = true;	rem = false;	stateOn();	console.log('Its Done Block');}remove.onclick = function() {	act = false;	dn = false;	rem = true;	stateOn();	console.log('Its remove Block');}function stateOn() {  stateIn = TodoStorage.data;  deleteLabel();  stateIn.forEach (function(item,i,arr) {  	    if (act) {      if (item.state == 'active') {	      labelTake();  		}    }    if (dn) {      if (item.state == 'done') {	      labelTake();	      x.setAttribute("checked", true);          lineThrough(label);  		}    }    if (rem) {      if (item.state == 'remove') {	      labelTake();  		}    }	function labelTake() {		link = document.createElement("li");		x = document.createElement("INPUT");		label = document.createElement("label");		x.setAttribute("type", "checkbox");		label.setAttribute("for", "c" + i);		label.setAttribute("id", "cs" + i);		x.setAttribute("id", "c" + i);		x.setAttribute("onclick", "changeBox(" + i + ")");		ul.appendChild(link).appendChild(x);		ul.appendChild(link).appendChild(label).innerHTML = item.title;			}		}	);}function deleteLabel() {	if (ul.childNodes.length > 0) {		for (i = 0; i < ul.childNodes.length; i++) {			x = document.getElementById("c" + i);			label = document.getElementById("cs" + i);			ul.removeChild(ul.childNodes[i]);			deleteLabel();		}	}}
--- script/script.js
+++ script/script.js
@@ -1,52 +1,42 @@
-var count = 0;
-var textBox = [];
+var textBox = [],
+press = document.getElementById("add"),
+text = document.getElementById("text"),
+block = document.getElementById("block"),
+blockOne = document.getElementById("blockOne"),
+ul = document.getElementById("ul");
 
-var press = document.getElementById("add");
-var text = document.getElementById("text");
-var block = document.getElementById("block");
-var blockOne = document.getElementById("blockOne");
-var ul = document.getElementById("ul");
-
-window.onload = Prov;
+window.onload = TodoStorage.create();
 press.onclick = init;
 
-/*  @ STATE : active, remove, done*/
+/*  @ STATE : active,
+ remove, done*/
 
 function init() {
   if (text.value === '' || text.value.length > 30) {
     alert("Вы не можете вводить пустую строку, или строку которая имеет больше 30 символов!");
     return false;
-  }
-  else if (TodoStorage.data.length > 0) {
-    TodoStorage.addItem({'title': strip_tags(text.value.trim()), 'check': false, 'state': 'active'});
+  } else if (TodoStorage.data.length > 0) {
+    TodoStorage.addItem({ 'title': strip_tags(text.value.trim()), 'check': false, 'state': 'active' });
     addVis(strip_tags(text.value.trim()));
     text.value = '';
-  }
-  else {
-    TodoStorage.addItem({'title': strip_tags(text.value.trim()), 'check': false, 'state': 'active'});
+  } else {
+    TodoStorage.addItem({ 'title': strip_tags(text.value.trim()), 'check': false, 'state': 'active' });
     addVis(strip_tags(text.value.trim()));
     text.value = '';
   }
 }
-
-function Prov() {
-  TodoStorage.create();
-}
-
 function addVis(text) {
   linkWithCheck();
-  x.setAttribute("id", "c" + Math.floor(TodoStorage.data.length - 1));
+  x.setAttribute("id", "c" + Math.floor(TodoStorage.data.length - 1) );
   x.setAttribute("onclick", "changeBox(" + Math.floor(TodoStorage.data.length - 1) + ")");
-  label.setAttribute("for", "c" + Math.floor(TodoStorage.data.length - 1));
-  label.setAttribute("id", "cs" + Math.floor(TodoStorage.data.length - 1));
+  label.setAttribute("for", "c" + Math.floor(TodoStorage.data.length - 1) );
+  label.setAttribute("id", "cs" + Math.floor(TodoStorage.data.length - 1) );
   ul.appendChild(link).appendChild(x);
   ul.appendChild(link).appendChild(label).innerHTML = text;
 }
-
 function strip_tags(str) {
   return str.replace(/<\/?[^>]+>/gi, '');
 }
-
 function changeBox(s) {
   labelS = document.getElementById("cs" + s);
   x = document.getElementById("c" + s);
@@ -58,8 +48,7 @@ function changeBox(s) {
     localStorage.setItem(TodoStorage.storageName, changeSet);
     lineThrough(labelS);
     console.log("['TodoStorage'] Цель выполнена " + chan[s].title);
-  }
-  else {
+  } else {
     chan[s].check = false;
     chan[s].state = 'active';
     changeSet = JSON.stringify(chan);
@@ -69,17 +58,14 @@ function changeBox(s) {
     console.log("['TodoStorage'] Цель была снята с выполненной задачи " + chan[s].title);
   }
 }
-
 function lineThrough(label) {
   label.style.textDecoration = "line-through";
   label.style.color = "rgb(177, 20, 20)";
   label.style.transition = "all 1s ease-out 0.5s";
 }
-
 function linkWithCheck() {
   link = document.createElement("li");
   x = document.createElement("INPUT");
   label = document.createElement("label");
   x.setAttribute("type", "checkbox");
-  return x, label, link;
 }
