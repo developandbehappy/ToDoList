c561bd1424d7c1ca85a6fd2b72da3fe7ed5c02b4
zidein07 <zidein07@gmail.com>
2015-07-29 12:11:26 +0400

    refactor Code

--- script/script.js
+++ script/script.js
@@ -1,71 +1,91 @@
-var textBox = [],
-press = document.getElementById("add"),
-text = document.getElementById("text"),
-block = document.getElementById("block"),
-blockOne = document.getElementById("blockOne"),
-ul = document.getElementById("ul");
+var textBox = [];
+var press = document.getElementById('add');
+var text = document.getElementById('text');
+var block = document.getElementById('block');
+var blockOne = document.getElementById('blockOne');
+var ul = document.getElementById('ul');
 
 window.onload = TodoStorage.create();
 press.onclick = init;
 
-/*  @ STATE : active,
- remove, done*/
-
+/**
+ * @ STATE : active, remove, done
+ * @returns {boolean}
+ */
 function init() {
   if (text.value === '' || text.value.length > 30) {
-    alert("Вы не можете вводить пустую строку, или строку которая имеет больше 30 символов!");
+    alert('Вы не можете вводить пустую строку, или строку которая имеет больше 30 символов!');
     return false;
   } else if (TodoStorage.data.length > 0) {
-    TodoStorage.addItem({ 'title': strip_tags(text.value.trim()), 'check': false, 'state': 'active' });
-    addVis(strip_tags(text.value.trim()));
+    TodoStorage.addItem({
+      title: stripTags(text.value.trim()),
+      check: false,
+      state: 'active'
+    });
+    addVisuale(stripTags(text.value.trim()));
     text.value = '';
   } else {
-    TodoStorage.addItem({ 'title': strip_tags(text.value.trim()), 'check': false, 'state': 'active' });
-    addVis(strip_tags(text.value.trim()));
+    TodoStorage.addItem({
+      title: stripTags(text.value.trim()),
+      check: false,
+      state: 'active'
+    });
+    addVisuale(stripTags(text.value.trim()));
     text.value = '';
   }
 }
-function addVis(text) {
+
+function addVisuale(text) {
   linkWithCheck();
-  x.setAttribute("id", "c" + Math.floor(TodoStorage.data.length - 1) );
-  x.setAttribute("onclick", "changeBox(" + Math.floor(TodoStorage.data.length - 1) + ")");
-  label.setAttribute("for", "c" + Math.floor(TodoStorage.data.length - 1) );
-  label.setAttribute("id", "cs" + Math.floor(TodoStorage.data.length - 1) );
+  dataStorage = Math.floor(TodoStorage.data.length - 1);
+  x.setAttribute('id', 'c' + dataStorage );
+  x.setAttribute('onclick', 'changeBox(' + dataStorage + ')');
+  label.setAttribute('for', 'c' + dataStorage);
+  label.setAttribute('id', 'cs' + dataStorage);
   ul.appendChild(link).appendChild(x);
   ul.appendChild(link).appendChild(label).innerHTML = text;
 }
-function strip_tags(str) {
+
+function stripTags(str) {
   return str.replace(/<\/?[^>]+>/gi, '');
 }
+
 function changeBox(s) {
-  labelS = document.getElementById("cs" + s);
-  x = document.getElementById("c" + s);
-  chan = TodoStorage.data;
+  var labelS = document.getElementById('cs' + s);
+  var x = document.getElementById('c' + s);
+  var chan = TodoStorage.data;
+  var changeSet = '';
   if (x.checked) {
     chan[s].check = true;
     chan[s].state = 'done';
     changeSet = JSON.stringify(chan);
     localStorage.setItem(TodoStorage.storageName, changeSet);
     lineThrough(labelS);
-    console.log("['TodoStorage'] Цель выполнена " + chan[s].title);
+    console.log('[TodoStorage] done -> ' + chan[s].title);
   } else {
     chan[s].check = false;
     chan[s].state = 'active';
     changeSet = JSON.stringify(chan);
     localStorage.setItem(TodoStorage.storageName, changeSet);
-    labelS.style.textDecoration = "none";
-    labelS.style.color = "#fff";
-    console.log("['TodoStorage'] Цель была снята с выполненной задачи " + chan[s].title);
+    labelS.style.textDecoration = 'none';
+    labelS.style.color = '#fff';
+    console.log('[TodoStorage] undone -> ' + chan[s].title);
   }
 }
+
 function lineThrough(label) {
-  label.style.textDecoration = "line-through";
-  label.style.color = "rgb(177, 20, 20)";
-  label.style.transition = "all 1s ease-out 0.5s";
+  label.style.textDecoration = 'line-through';
+  label.style.color = 'rgb(177, 20, 20)';
+  label.style.transition = 'all 1s ease-out 0.5s';
+  return label;
 }
+
 function linkWithCheck() {
-  link = document.createElement("li");
-  x = document.createElement("INPUT");
-  label = document.createElement("label");
-  x.setAttribute("type", "checkbox");
+  var x = document.createElement('INPUT');
+  var label = document.createElement('label');
+  x.setAttribute('type', 'checkbox');
+  return {
+    x: x,
+    label: label
+  };
 }
