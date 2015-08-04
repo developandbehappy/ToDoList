762b97e795c178f42e30890a1531a01e806b8cb1
zidein07 <zidein07@gmail.com>
2015-08-04 16:55:15 +0400

    gulp change

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,7 @@ var gulp = require('gulp'),
 
 gulp.task('lint', function () {
 gulp.src('script/*.js')
-    .pipe(eslint({
-        rules: {
-            'my-custom-rule': 1,
-            'strict': 2,
-            "camelcase": 1,
-            "comma-dangle": 2,
-            "quotes": 0
-        },
-        globals: {
-            'jQuery':false,
-            '$':true
-        },
-        envs: [
-            'browser'
-        ]
-    }))
+    .pipe(eslint())
     .pipe(eslint.formatEach('compact', process.stderr));
 });
 
