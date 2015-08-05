876dc8fc38f03a2308a34e840924173e4f387d9e
zidein07 <zidein07@gmail.com>
2015-08-05 15:52:22 +0400

    new commit for a test

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 var gulp = require('gulp'),
-    eslint = require('gulp-eslint');
+  connect = require('gulp-connect'),
+  elsint = require('gulp-eslint');
 
-gulp.task('lint', function () {
-gulp.src('script/*.js')
-    .pipe(eslint())
-    .pipe(eslint.formatEach('compact', process.stderr));
+gulp.task('connectDev', function () {
+  connect.server({
+    root: ['app', 'tmp'],
+    port: 8000,
+    livereload: true
+  });
 });
 
-
-gulp.task('default', ['lint'], function () {
- 
+gulp.task('connectDist', function () {
+  connect.server({
+    root: 'dist',
+    port: 8001,
+    livereload: true
+  });
 });
 
+gulp.task('html', function () {
+  gulp.src('*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['index.html'], ['html']);
+});
+
+gulp.task('default', ['connectDist', 'connectDev', 'watch']);
